3a0b5c91ea0a44b8c1f1a37d06f537522a1adf5c
darenzai <1453566224@qq.com>
2019-05-11 14:41:35 +0800

    first

---
+++ src/com/daenzai/entitly/TestSpring.java
@@ -0,0 +1,25 @@
+package com.daenzai.entitly;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class TestSpring {
+
+
+    public  static void main(String []args){
+
+
+        ApplicationContext context=new ClassPathXmlApplicationContext("applicationConext.xml");
+
+        Categlory cat=(Categlory) context.getBean("c");
+
+        System.out.println(cat.name);
+
+
+    }
+
+
+
+
+
+}
